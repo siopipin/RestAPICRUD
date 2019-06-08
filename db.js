eb14829e1305cd73e0b465f009677e9d0f444987
sio <siojurnalispipin@hotmail.com>
2019-06-08 10:02:52 +0700

    update port2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 const uri = process.env.NODE_ENV === 'production' ?
   "mongodb+srv://dbSio:sio@dbcloud-rvfh0.mongodb.net/gps_mobile?retryWrites=true":
-  "mongodb://db:27017/gps_mobile"
+  "mongodb://db:27018/gps_mobile"
 
 mongoose.connect(uri, { useNewUrlParser: true}, function(err){
   if(err){
