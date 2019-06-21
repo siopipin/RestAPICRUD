7dbd78c405ef1d4edb7faf1b25d1d5ef3dc5951c
sio <siojurnalispipin@hotmail.com>
2019-06-21 10:44:59 +0700

    update with aktivitas transaksi controller

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
-var db = require("./db");
 
 var UserController = require("./controller/PelangganController");
 var RingkasanController = require("./controller/RingkasanController");
 var JenisTransaksi = require("./controller/JenisTransaksiController");
+var AktivitasTransaksi = require("./controller/AktivitasTransController");
 
 app.use("/api", UserController);
 app.use("/api", RingkasanController);
-app.use("/api", JenisTransaksi);
+app.use("/api", AktivitasTransaksi);
 module.exports = app;
