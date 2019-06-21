2affd7d8ce8d5696c1edcf199af49f1685fa68dc
Sio Jurnalis Pipin <siojurnalispipin@hotmail.com>
2019-06-21 13:43:31 +0700

    aktivitas transaksi

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var db = require('./db');
 var UserController = require("./controller/PelangganController");
 var RingkasanController = require("./controller/RingkasanController");
 var JenisTransaksi = require("./controller/JenisTransaksiController");
+var AktivitasTransaksi = require("./controller/AktivitasTransController");
 
 app.use("/api", UserController);
 app.use("/api", RingkasanController);
 app.use("/api", JenisTransaksi);
+app.use("/api", AktivitasTransaksi);
 module.exports = app;
---
+++ model/AktivitasTransaksi.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+var SkemaAktivitasTransaksi = new mongoose.Schema({
+  nama_aktivitas: String,
+  kode: String,
+  tujuan: String,
+  nominal: Number,
+  tanggal_transaksi: Date
+});
+
+mongoose.model("AktivitasTransaksi", SkemaAktivitasTransaksi);
+module.exports = mongoose.model("AktivitasTransaksi");
