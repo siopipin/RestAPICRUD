b60bc36ff43daa06e12ef01f8ff508e1408543e9
sio <siojurnalispipin@hotmail.com>
2019-06-21 13:16:58 +0700

    update

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var app = express();
 
 var UserController = require("./controller/PelangganController");
 var RingkasanController = require("./controller/RingkasanController");
-var JenisTransaksi = require("./controller/JenisTransaksiController");
-var AktivitasTransaksi = require("./controller/AktivitasTransController");
 
 app.use("/api", UserController);
 app.use("/api", RingkasanController);
-app.use("/api", AktivitasTransaksi);
 module.exports = app;
--- model/JenisTransaksi.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-var SkemaTransaksi = new mongoose.Schema({
-  nama_transaksi: String,
-  kode: String,
-  saldo: Number
-});
-
-mongoose.model("JenisTransaksi", SkemaTransaksi);
-module.exports = mongoose.model("JenisTransaksi");
