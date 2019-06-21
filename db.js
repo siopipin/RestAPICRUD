45f7e881f19205b5d4faab5d76e3309abde4c326
sio <siojurnalispipin@hotmail.com>
2019-06-21 12:43:27 +0700

    update database3

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-const uri = "mongodb://127.0.0.1:27017/dbmobile"
+const uri = "mongodb+srv://dbSio:sio@dbcloud-rvfh0.mongodb.net/gps_mobile?retryWrites=true"
 mongoose.connect(uri, { useNewUrlParser: true}, function(err){
   if(err){
     throw err
